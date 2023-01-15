7706edbf05b28b0f63e1b6d151953bd85384d9d9
Guliveer <oliwer.pawelski@gmail.com>
2023-01-15 12:10:32 +0100

    Fix

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
-//? EDIT BELOW THIS LINE
+//? EDIT BELOW
 
 var _vcfURL = "https://guliveer.github.io/vCard/contact.vcf";
 var _firstNAME = "Oliwer";
 var _lastNAME = "Pawelski";
 var _nick = "Chińczyk";
-var _birthday =  "01.05.2003";
-var _phone =  "+48 884 857 188";
+var _birthday = "01.05.2003";
+var _phone = "+48 884 857 188";
 var _email = "oliwer.pawelski@gmail.com";
 var _links = {
-    // "Name": "URL"
-    'Facebook': 'https://facebook.com/O.Pawelski',
-    'LinkedIn': 'https://linkedin.com/in/oliwer-pawelski',
-    'Moja Witryna': 'https://guliveer.github.io',
-    'GitHub': 'https://github.com/Guliveer'
+	// "Name": "URL"
+	"Facebook": "https://facebook.com/O.Pawelski",
+	"LinkedIn": "https://linkedin.com/in/oliwer-pawelski",
+	"Moja Witryna": "https://guliveer.github.io",
+	"GitHub": "https://github.com/Guliveer",
 };
