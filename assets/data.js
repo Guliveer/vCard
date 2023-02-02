18c22a459eb25d51e97d217a7df023c20f738f4f
Guliveer <oliwer.pawelski@gmail.com>
2023-02-02 23:09:08 +0100

    Website rework

---
+++ assets/data.js
@@ -0,0 +1,32 @@
+//! DO NOT EDIT ANYTHING BELOW THIS LINE
+// Insert _vcfURL as src attribute into #vcf-url
+$("#qr").attr("src", "https://api.qrserver.com/v1/create-qr-code/?data=" + _vcfURL + "&color=fff&bgcolor=000&margin=0&format=svg");
+
+// Insert _firstNAME and _lastNAME
+$("#text01").text(_firstNAME + " " + _lastNAME);
+
+// Insert _nick
+if (_nick != "") {
+	$("#text07").text('"' + _nick + '"');
+}
+
+// Insert _phone
+$("#phone").text(_phone);
+$("#phone-a").attr("href", "tel:" + _phone);
+
+// Insert _email
+$("#email").text(_email);
+$("#email-a").attr("href", "mailto:" + _email);
+
+// Insert _links
+var items = [];
+$.each(_links, function (key, val) {
+	items.push("<li>" + "<a href='" + val + "' target='_blank' class='button'>" + key + "</a></li>");
+});
+$("#buttons03").empty().append(items.join(""));
+
+// Edit default download file name
+$("#add-contact").attr("download", _firstNAME + "-" + _lastNAME + ".vcf");
+
+// Set website title
+document.title = _firstNAME + " " + _lastNAME + " | vCard";
--- config.js
+++ config.js
@@ -4,13 +4,12 @@ var _vcfURL = "https://guliveer.github.io/vCard/contact.vcf";
 var _firstNAME = "Oliwer";
 var _lastNAME = "Pawelski";
 var _nick = "Chińczyk";
-var _birthday = "01.05.2003";
 var _phone = "+48 884 857 188";
 var _email = "oliwer.pawelski@gmail.com";
 var _links = {
 	// "Name": "URL"
 	"Facebook": "https://facebook.com/O.Pawelski",
 	"LinkedIn": "https://linkedin.com/in/oliwer-pawelski",
-	"Moja Witryna": "https://guliveer.github.io",
 	"GitHub": "https://github.com/Guliveer",
+	"Zobacz więcej...": "https://guliveer.github.io",
 };
