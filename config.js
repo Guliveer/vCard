7fb64e4e5645e094227c9d7c6de9ea05b386a557
Guliveer <oliwer.pawelski@gmail.com>
2023-02-03 13:45:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ var _nick = "Chińczyk";
 var _phone = "+48 884 857 188";
 var _email = "oliwer.pawelski@gmail.com";
 var _links = {
-	// "Name": "URL"
+	// "Name": "URL",
 	"Facebook": "https://facebook.com/O.Pawelski",
 	"LinkedIn": "https://linkedin.com/in/oliwer-pawelski",
 	"GitHub": "https://github.com/Guliveer",
-	"Zobacz więcej...": "https://guliveer.github.io",
+	"Zobacz więcej...": "https://guliveer.github.io/links",
 };
